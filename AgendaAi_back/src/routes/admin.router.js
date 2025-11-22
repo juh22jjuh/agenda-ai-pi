a6dfb19f81684f832dfb1df575b08bc63e420ce1
anajuliamartins468 <anajuliamartins468@gmail.com>
2025-11-22 20:49:56 +0000

    ...

--- AgendaAi_back/src/routes/admin.router.js
+++ AgendaAi_back/src/routes/admin.router.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { listUsers, deleteUser, deactivateUser, activateUser } from "../controllers/admin.controller.js";
 import { authorize } from "../utils/authorize.js";
-import { authMiddleware } from "../utils/authMiddleware.js";
+import { auth } from "../utils/auth.js";
 
 export const routerAdmin = Router();
 
-routerAdmin.get("/admin/users", authMiddleware, authorize(["admin"])
+routerAdmin.get("/users", auth, authorize(["admin"])
 , listUsers);
-routerAdmin.delete("/admin/users/:id", authMiddleware, authorize(["admin"])
+routerAdmin.delete("/users/:id", auth, authorize(["admin"])
 , deleteUser);
-routerAdmin.put("/admin/users/:id/deactivate", authMiddleware, authorize(["admin"])
+routerAdmin.put("/users/:id/deactivate", auth, authorize(["admin"])
 , deactivateUser);
-routerAdmin.put("/admin/users/:id/activate", authMiddleware, authorize(["admin"])
+routerAdmin.put("/users/:id/activate", auth, authorize(["admin"])
 , activateUser);
