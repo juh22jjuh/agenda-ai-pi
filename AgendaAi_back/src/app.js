f0adc1bc24a1e4c772ab895af8607ca5e6443c82
anajuliamartins468 <anajuliamartins468@gmail.com>
2025-11-22 20:45:34 +0000

    atualização

--- AgendaAi_back/src/app.js
+++ AgendaAi_back/src/app.js
@@ -3,7 +3,7 @@ import {router} from "./routes/user.route.js";
 import {routerEnt} from "./routes/entrepreneur.routes.js";
 import dotenv from 'dotenv/config.js'
 import { routerCont } from './routes/contato.router.js';
-//import { routerAdmin } from './routes/admin.route.js';
+import { routerAdmin } from './routes/admin.route.js';
 import { routerServicesEntreprenuer } from './routes/services_entreprenuer.js';
 import { routerSche } from './routes/scheduling.router.js';
 import cors from 'cors';
@@ -32,3 +32,4 @@ app.use("/contato", routerCont);
 app.use("/servicesEntreprenuer", routerServicesEntreprenuer);
 app.use("/api/entrepreneur", routerEnt);
 app.use("/api/user", router);
+app.use("/admin", routerAdmin);
