10c9d83c194841cf42ebe16047a0ea8b9add6eab
anajuliamartins468 <anajuliamartins468@gmail.com>
2025-11-22 21:05:29 +0000

    admin correção

--- AgendaAi_back/src/controllers/admin.controller.js
+++ AgendaAi_back/src/controllers/admin.controller.js
@@ -3,7 +3,7 @@ import { User } from "../models/User.js";
 export const listUsers = async (req, res) => {
   try {
     const users = await User.find({}, "-password"); // não mostra a senha
-    res.json(users);
+    res.json(users || []);
   } catch (error) {
     res.status(500).json({ error: "Erro ao listar usuários" });
   }
