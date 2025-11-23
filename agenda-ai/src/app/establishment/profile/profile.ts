56879ed88b3135899c10a2665ebd9711144e318b
anajuliamartins468 <anajuliamartins468@gmail.com>
2025-11-23 08:50:15 +0000

    ....

--- agenda-ai/src/app/establishment/profile/profile.ts
+++ agenda-ai/src/app/establishment/profile/profile.ts
@@ -145,6 +145,7 @@ export class Profile implements OnInit {
       tap(({ services, schedules }) => {
         this.services = services;
         this.schedules = schedules;
+        console.log('Agendamentos recebidos:', schedules);
       })
     );
   }
