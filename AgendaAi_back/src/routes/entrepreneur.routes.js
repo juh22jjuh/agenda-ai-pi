919fd245e492d660e3c0587a2baafafa68673d2d
Brunovini08 <brunovinicius082005@gmail.com>
2025-11-23 02:29:13 -0300

    fix: Resolvendo problemas da controller de empresa

--- AgendaAi_back/src/routes/entrepreneur.routes.js
+++ AgendaAi_back/src/routes/entrepreneur.routes.js
@@ -1,17 +1,17 @@
 
 import { Router } from 'express';
 // Adiciona a nova função ao import
-import { Register, Login, getEntrepreneurByUserId } from '../controllers/entrepreneur.controller.js';
+import { Register, AllEntrepreneur, Delete, GetEntreprenuerById, SearchEntrepreneurByName, ToggleStatusEntrepreneur, UpdateEntreprenuer, getEntrepreneurByUserId } from '../controllers/entrepreneur.controller.js';
 import upload from '../config/multer.js'; // Import the multer configuration
-
+import { authorize } from "../utils/authorize.js"
 export const routerEnt = Router();
 
 // Rota para registro de empreendedor
 routerEnt.post('/register/:userId', upload.single('companyImage'), Register);
-
-// Rota para login
-routerEnt.post('/login', Login);
-
-// --- NOVA ROTA ADICIONADA ---
-// Rota para buscar dados do empreendedor pelo ID do usuário
+routerEnt.delete("/delete/:id", Delete, authorize('entrepreneur'))
+routerEnt.get("/entreprenuers", AllEntrepreneur)
+routerEnt.get("/entrepreneur/:id", GetEntreprenuerById)
+routerEnt.patch("/update/:id", UpdateEntreprenuer)
+routerEnt.patch("/status/:id", ToggleStatusEntrepreneur)
+routerEnt.get("/search", SearchEntrepreneurByName)
 routerEnt.get('/user/:userId', getEntrepreneurByUserId);
