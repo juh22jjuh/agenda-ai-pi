9e9269492ddecd92c9af5859be9319eb02d91ca7
juh22jjuh <anajuliamartins468@gmail.com>
2025-11-23 01:15:01 -0300

    Corrigindo problemas de importação

--- AgendaAi_back/src/controllers/entrepreneur.controller.js
+++ AgendaAi_back/src/controllers/entrepreneur.controller.js
@@ -1,5 +1,5 @@
 
-import User from '../models/User.js';
+import { User } from '../models/User.js';
 
 export const Register = async (req, res) => {
   const { userId } = req.params;
--- AgendaAi_back/src/routes/entrepreneur.routes.js
+++ AgendaAi_back/src/routes/entrepreneur.routes.js
@@ -3,13 +3,12 @@ import { Router } from 'express';
 import { Register, Login } from '../controllers/entrepreneur.controller.js';
 import upload from '../config/multer.js'; // Import the multer configuration
 
-const router = Router();
+export const routerEnt = Router();
 
 // Use the 'upload' middleware on the registration route.
 // 'upload.single("companyImage")' tells multer to expect a single file in a field named "companyImage".
 // This name MUST match the name used in the frontend FormData.
-router.post('/register/:userId', upload.single('companyImage'), Register);
+routerEnt.post('/register/:userId', upload.single('companyImage'), Register);
 
-router.post('/login', Login);
+routerEnt.post('/login', Login);
 
-export default router;
